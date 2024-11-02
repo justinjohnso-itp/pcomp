c62262c31039fa919855f6f73308f7069b731d27
Justin <justin@jjohnson.io>
2024-11-01 21:15:48 -0400

    add p5.js setup

--- week-7-homework/src/main.cpp
+++ week-7-homework/src/main.cpp
@@ -1,54 +1,62 @@
 #include <Arduino.h>
 
-const int switchPin = 2; // digital input
+//
+// Intro to Asynchronous Serial Communications
+// https://itp.nyu.edu/physcomp/lab-intro-to-serial-communications/
+//
 
-void setup() {
-   // start serial port at 9600 bps:
-   Serial.begin(9600);
-   pinMode(switchPin, INPUT);
+// const int switchPin = 2; // digital input
 
-   // Handshake
-   while (Serial.available() <= 0) {
-     Serial.println("hello"); // send a starting message
-     delay(300);              // wait 1/3 second
-   }
-}
+// void setup() {
+//    // start serial port at 9600 bps:
+//    Serial.begin(9600);
+//    pinMode(switchPin, INPUT);
 
-/* Sending data in many formats (https://itp.nyu.edu/physcomp/lab-intro-to-serial-communications/#send-the-data-in-many-formats)*/
-/*
-void loop() {
-// read analog input, map it to make the range 0-255:
-int analogValue = analogRead(A0);
-int mappedValue = map(analogValue, 0, 1023, 0, 255);
-Serial.println(mappedValue);
+//    // Handshake
+//    while (Serial.available() <= 0) {
+//      Serial.println("hello"); // send a starting message
+//      delay(300);              // wait 1/3 second
+//    }
+// }
+
+// Sending data in many formats (https://itp.nyu.edu/physcomp/lab-intro-to-serial-communications/#send-the-data-in-many-formats)
+// void loop() {
+// // read analog input, map it to make the range 0-255:
+// int analogValue = analogRead(A0);
+// int mappedValue = map(analogValue, 0, 1023, 0, 255);
+// Serial.println(mappedValue);
  
-// print different formats:
-Serial.write(mappedValue);  // Print the raw binary value
-Serial.print('\t');             // print a tab
-// print ASCII-encoded values:
-Serial.print(mappedValue, BIN); // print ASCII-encoded binary value
-Serial.print('\t');             // print a tab
-Serial.print(mappedValue);      // print decimal value
-Serial.print('\t');             // print a tab
-Serial.print(mappedValue, HEX); // print hexadecimal value
-Serial.print('\t');             // print a tab
-Serial.print(mappedValue, OCT); // print octal value
-Serial.println();               // print linefeed and carriage return
+// // print different formats:
+// Serial.write(mappedValue);  // Print the raw binary value
+// Serial.print('\t');             // print a tab
+// // print ASCII-encoded values:
+// Serial.print(mappedValue, BIN); // print ASCII-encoded binary value
+// Serial.print('\t');             // print a tab
+// Serial.print(mappedValue);      // print decimal value
+// Serial.print('\t');             // print a tab
+// Serial.print(mappedValue, HEX); // print hexadecimal value
+// Serial.print('\t');             // print a tab
+// Serial.print(mappedValue, OCT); // print octal value
+// Serial.println();               // print linefeed and carriage return
 
-delay(50);
-}
-*/
+// delay(50);
+// }
 
-void loop() {
-   if (Serial.available()) {
-      Serial.print(analogRead(A0));
-      Serial.print(",");
+// void loop() {
+//    if (Serial.available()) {
+//       Serial.print(analogRead(A0));
+//       Serial.print(",");
 
-      Serial.print(analogRead(A1));
-      Serial.print(",");
+//       Serial.print(analogRead(A1));
+//       Serial.print(",");
    
-      Serial.println(digitalRead(switchPin));
+//       Serial.println(digitalRead(switchPin));
 
-      delay(50);
-   }
-}
+//       delay(50);
+//    }
+// }
+
+// Serial Input to p5.js Using the p5.webserial Library
+// https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-webserial-input-to-p5-js/
+//
+//
