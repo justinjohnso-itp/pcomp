521f85ec9e8b8b707c2d5e8bfd83c54533753d6e
Justin <justin@jjohnson.io>
2024-11-05 11:53:15 -0500

    add more exercises

--- week-10-class/src/main.cpp
+++ week-10-class/src/main.cpp
@@ -3,15 +3,54 @@
 
 Servo myServo;
 
+//
+// P5 to Arduino
+//
+
+// void setup() {
+//   Serial.begin(9600);
+//   myServo.attach(11);
+// }
+// void loop() {
+//   if(Serial.available()){
+//   byte byteFromSerial = Serial.read();
+//   int mappedValue = map(byteFromSerial,0,100,0,170);
+//   analogWrite(3,mappedValue);
+//   myServo.write(mappedValue);
+//   }
+// }
+
+//
+// Multiple values with parseInt()
+//
+// void setup() {
+//   Serial.begin(9600);
+//   myServo.attach(11);
+// }
+// void loop() {
+//   if (Serial.available()) {
+    
+//     // read first part of string as int
+//     int fromSerial = Serial.parseInt();
+//     analogWrite(3, fromSerial);
+    
+//     // read second part of string as int
+//     fromSerial = Serial.parseInt();
+//     analogWrite(5, fromSerial);   
+//   }
+// }
+
+//
+// Multiple ASCII values, Arduino to P5
+//
 void setup() {
   Serial.begin(9600);
-  myServo.attach(11);
 }
 void loop() {
-  if(Serial.available()){
-  byte byteFromSerial = Serial.read();
-  int mappedValue = map(byteFromSerial,0,100,0,170);
-  analogWrite(3,mappedValue);
-  myServo.write(mappedValue);
-  }
+  int valueToSend = analogRead(A0);
+  Serial.print(valueToSend);
+  Serial.print(",");
+  valueToSend = analogRead(A1);
+  Serial.println(valueToSend);
+  delay (50);
 }
